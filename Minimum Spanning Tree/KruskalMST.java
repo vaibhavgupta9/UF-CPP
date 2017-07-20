4fbab6f18e4c78098769da04650ad8ced88e4c8d
TropicalNerd <praenubilus@gmail.com>
2017-07-20 02:00:26 -0400

    add comments

--- Minimum Spanning Tree/KruskalMST.java
+++ Minimum Spanning Tree/KruskalMST.java
@@ -2,6 +2,11 @@ import java.util.ArrayList;
 
 /**
  * Created by chris on 7/18/17.
+ * The time complexity will not count the time of conversion between different graph representations
+ * (e.g. from matrix to edges list or vertices list). Kruskal's algorithm implementation will take O(ELogE) to
+ * sort all the edges, and the Union Find in this code implemented through weighted union find with paths
+ * compression, which will take O(V) time to build the data structure and almost O(1) amortized time for
+ * union and find operation. So the total time complexity is O(ELogE) and O(E+V) space complexity
  */
 public class KruskalMST extends MST {
 
